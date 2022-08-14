0751c7234a113e446ba657e8825fdf9bb47eece6
J-Boii <jamieb2684@gmail.com>
2022-08-14 22:13:01 +0100

    Add scale option to shape

--- shapes.c
+++ shapes.c
@@ -4,6 +4,12 @@ void drawH(int x);
 void block(int x);
 void space(int x);
 int getInput(void);
+void topRow(int x);
+void midRow(int x);
+
+
+// TASK: Fix format issue when x is 20
+// TASK: add other characters
 
 
 int main(void) {
@@ -24,7 +30,7 @@ for (int j = 0; j < x/3; j++) {
         printf(" ");
     }
 }
-
+/*
 void drawH(int x) {
     block(x);
     space(x);
@@ -40,6 +46,7 @@ void drawH(int x) {
     printf("\n");
     
 }
+*/
 
 int getInput(void) {
     printf("Desired X value: \n");
@@ -47,3 +54,60 @@ int getInput(void) {
     scanf("%i", &a);
     return a;
 }
+
+void drawH(int x) {
+    topRow(x);
+    midRow(x);
+    topRow(x);
+}
+
+void topRow(int x){
+    for (int i = 0; i < x/3; i++){
+    for (int i = 0; i < x/3; i++){
+        block(x/3);
+    }
+    for (int i = 0; i < x/3; i++){
+        space(x/3);
+    }
+    for (int i = 0; i < x/3; i++){
+        block(x/3);
+    }
+    printf("\n");
+    }
+}
+
+void midRow(int x){
+    for (int i = 0; i < x/3; i++){
+    for (int i = 0; i < x; i++){
+        block(x/3);
+    }
+    printf("\n");
+    }
+}
+
+
+
+/*
+
+repeat x/3 times
+    repeat x/3 times
+        block
+    repeat x/3 times
+        space
+    repeat x/3 times
+        block
+    new line
+repeat x/3 times
+    repeat x/3 times
+        block
+    new line
+repeat x/3 times
+    repeat x/3 times
+        block
+    repeat x/3 times
+        space
+    repeat x/3 times
+        block
+    new line
+
+*/
